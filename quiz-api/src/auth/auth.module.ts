7b4f9316238cd44ec504dde49039afa353cb94ca
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2025-02-07 18:41:29 +0200

    feat: replace dotenv with @nestjs/config and add JwtModule register

--- quiz-api/src/auth/auth.module.ts
+++ quiz-api/src/auth/auth.module.ts
@@ -3,18 +3,9 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { JwtModule } from '@nestjs/jwt';
-import { config } from 'dotenv';
-import { resolve } from 'path';
-
-config({ path: resolve(__dirname, '../../.env') });
 
 @Module({
-  imports: [
-    UsersModule,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET,
-    }),
-  ],
+  imports: [UsersModule, JwtModule.register({})],
   controllers: [AuthController],
   providers: [AuthService],
 })
