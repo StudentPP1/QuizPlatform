834179fffff96a330c70ef82b3fb754f42e97cf4
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2025-02-07 16:14:37 +0200

    feat: add config and imports

--- quiz-api/src/auth/auth.module.ts
+++ quiz-api/src/auth/auth.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
+import { config } from 'dotenv';
+import { resolve } from 'path';
+
+config({ path: resolve(__dirname, '../../.env') });
 
 @Module({
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
