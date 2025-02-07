9c86d0c8b528d8b4597729269bda022750625fff
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2025-02-07 19:52:39 +0200

    feat: remove jwt module and add token module

--- quiz-api/src/auth/auth.module.ts
+++ quiz-api/src/auth/auth.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { UsersModule } from 'src/users/users.module';
-import { JwtModule } from '@nestjs/jwt';
+import { UsersModule } from '../users/users.module';
+import { TokenModule } from '../token/token.module';
 
 @Module({
-  imports: [UsersModule, JwtModule.register({})],
+  imports: [UsersModule, TokenModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
