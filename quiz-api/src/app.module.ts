34976f61ad5acaf9a03201e6f5f02420c6270077
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2025-02-07 15:04:09 +0200

    feat: add database config

--- quiz-api/src/app.module.ts
+++ quiz-api/src/app.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { config } from 'dotenv';
+import { resolve } from 'path';
+
+config({ path: resolve(__dirname, '../.env') });
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      autoLoadEntities: true,
+      synchronize: process.env.NODE_ENV !== 'production',
+    }),
+    AuthModule,
+  ],
 })
 export class AppModule {}
