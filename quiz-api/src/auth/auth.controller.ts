fcf60da63e93ea7a09dfd9188cb79d2d99ff2b58
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2025-02-07 18:05:46 +0200

    feat: make method async

--- quiz-api/src/auth/auth.controller.ts
+++ quiz-api/src/auth/auth.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../users/dto/create-user.dto';
+import { Tokens } from './interfaces/tokens.interface';
 
 @Controller('api/auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.authService.create(createUserDto);
+  async create(@Body() createUserDto: CreateUserDto) {
+    return await this.authService.create(createUserDto);
   }
 }
