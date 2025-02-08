4babc67f63a2f5ba7af58e5f27f9e17636ec9742
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2025-02-08 15:10:06 +0200

    feat: create method fot google auth and update regular creating method

--- quiz-api/src/users/users.service.ts
+++ quiz-api/src/users/users.service.ts
@@ -3,6 +3,7 @@ import { CreateUserDto } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import { CreateGoogleUserDto } from './dto/create-google-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -20,6 +21,17 @@ export class UsersService {
     user.username = createUserDto.username;
     user.email = createUserDto.email;
     user.password = createUserDto.password;
+    user.authProvider = 'local';
+
+    return this.usersRepository.save(user);
+  }
+
+  async createGoogleUser(createGoogleUserDto: CreateGoogleUserDto) {
+    const user = new User();
+    user.username = createGoogleUserDto.username;
+    user.email = createGoogleUserDto.email;
+    user.googleId = createGoogleUserDto.googleId;
+    user.authProvider = 'google';
 
     return this.usersRepository.save(user);
   }
