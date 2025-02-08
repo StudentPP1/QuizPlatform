989bfef5bbc36f669c96f9c2984a4d34eee7408e
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2025-02-08 15:10:34 +0200

    feat: add logic for /google routes

--- quiz-api/src/auth/auth.controller.ts
+++ quiz-api/src/auth/auth.controller.ts
@@ -7,12 +7,15 @@ import {
   Req,
   ForbiddenException,
   Get,
+  UseGuards,
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../users/dto/create-user.dto';
 import { LoginDto } from './dto/login.dto';
 import { Request, Response } from 'express';
 import { TokenService } from '../token/token.service';
+import { AuthGuard } from '@nestjs/passport';
+import { User } from 'src/users/entities/user.entity';
 
 @Controller('api/auth')
 export class AuthController {
@@ -35,6 +38,25 @@ export class AuthController {
   async login(@Body() loginDto: LoginDto, @Res() res: Response) {
     const { accessToken, refreshToken } =
       await this.authService.login(loginDto);
+
+    this.tokenService.setRefreshTokenCookie(res, refreshToken);
+    return res.json({ accessToken });
+  }
+
+  @Get('google')
+  @UseGuards(AuthGuard('google'))
+  async googleAuth(@Req() req: Request) {
+    return;
+  }
+
+  @Get('google/redirect')
+  @UseGuards(AuthGuard('google'))
+  async googleAuthRedirect(
+    @Req() req: Request & { user: User },
+    @Res() res: Response,
+  ) {
+    const { accessToken, refreshToken } =
+      await this.authService.googleLogin(req);
 
     this.tokenService.setRefreshTokenCookie(res, refreshToken);
     return res.json({ accessToken });
