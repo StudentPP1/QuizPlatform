600783a0f5b9f8cd9a8dd5665ce03511f707c45c
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2025-02-09 13:41:45 +0200

    feat: add user interface

--- quiz-api/src/auth/strategies/jwt.strategy.ts
+++ quiz-api/src/auth/strategies/jwt.strategy.ts
@@ -3,6 +3,7 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Payload } from '../interfaces/payload.interface';
+import { User } from '../../users/interfaces/user.interface';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -14,7 +15,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: Payload) {
+  async validate(payload: Payload): Promise<User> {
     return {
       id: payload.sub,
       username: payload.username,
