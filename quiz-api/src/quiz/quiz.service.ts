efd168a75bd60c8225e250334f5c56d118cd9c99
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2025-02-10 16:48:25 +0200

    feat: update creating quiz logic

--- quiz-api/src/quiz/quiz.service.ts
+++ quiz-api/src/quiz/quiz.service.ts
@@ -5,7 +5,6 @@ import { Quiz } from './entities/quiz.entity';
 import { Repository } from 'typeorm';
 import { Task } from './entities/task.entity';
 import { Request } from 'express';
-import { UsersService } from '../users/users.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
@@ -15,7 +14,6 @@ export class QuizService {
     private readonly quizRepository: Repository<Quiz>,
     @InjectRepository(Task)
     private readonly taskRepository: Repository<Task>,
-    private readonly usersService: UsersService,
   ) {}
 
   async createQuiz(
@@ -23,22 +21,22 @@ export class QuizService {
     tasks: CreateTaskDto[],
     req: Request,
   ) {
-    const user = await this.usersService.findUserByEmail(req.user.email);
-
     const quiz = this.quizRepository.create({
       ...createQuizDto,
-      creator: user,
+      creator: req.user.id,
     });
 
     await this.quizRepository.save(quiz);
 
-    for (const taskDto of tasks) {
+    const taskEntities = tasks.map((taskDto) => {
       const task = this.taskRepository.create({
         ...taskDto,
         quiz: quiz,
       });
-      await this.taskRepository.save(task);
-    }
+      return task;
+    });
+
+    await this.taskRepository.save(taskEntities);
 
     return quiz;
   }
