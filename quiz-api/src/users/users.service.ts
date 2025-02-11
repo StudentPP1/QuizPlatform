bd9d29614dbec54387c8b687ce00a506fae4e44c
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2025-02-11 18:14:35 +0200

    feat: add update author rating method

--- quiz-api/src/users/users.service.ts
+++ quiz-api/src/users/users.service.ts
@@ -4,12 +4,15 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
 import { CreateGoogleUserDto } from './dto/create-google-user.dto';
+import { Review } from '../review/entities/review.entity';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User)
     private readonly usersRepository: Repository<User>,
+    @InjectRepository(Review)
+    private readonly reviewRepository: Repository<Review>,
   ) {}
 
   async findUserByEmail(email: string): Promise<User> {
@@ -33,5 +36,24 @@ export class UsersService {
     });
 
     return this.usersRepository.save(user);
+  }
+
+  async updateAuthorRating(userId: string): Promise<void> {
+    const reviews = await this.reviewRepository.find({
+      where: { quiz: { creator: { id: userId } } },
+    });
+
+    if (reviews.length === 0) {
+      return;
+    }
+
+    const totalRating = reviews.reduce((sum, review) => sum + review.rating, 0);
+    const averageRating = totalRating / reviews.length;
+
+    const user = await this.usersRepository.findOne({ where: { id: userId } });
+    if (user) {
+      user.authorRating = averageRating;
+      await this.usersRepository.save(user);
+    }
   }
 }
