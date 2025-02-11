c960a161494d9c2eac9d2d5a1b7b12330b702691
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2025-02-11 18:15:08 +0200

    feat: add review import and config

--- quiz-api/src/users/users.module.ts
+++ quiz-api/src/users/users.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { Review } from '../review/entities/review.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Review])],
   providers: [UsersService],
   exports: [UsersService],
 })
