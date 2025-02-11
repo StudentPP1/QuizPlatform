8e39c08585384ed8b08871ba719ad3333f8537be
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2025-02-11 18:16:58 +0200

    feat: create review resources

---
+++ quiz-api/src/review/review.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Quiz } from '../quiz/entities/quiz.entity';
+import { User } from '../users/entities/user.entity';
+import { Repository } from 'typeorm';
+import { CreateReviewDto } from './dto/create-review.dto';
+import { Review } from './entities/review.entity';
+import { Request } from 'express';
+import { UsersService } from '../users/users.service';
+
+@Injectable()
+export class ReviewService {
+  constructor(
+    @InjectRepository(Review)
+    private readonly reviewRepository: Repository<Review>,
+    @InjectRepository(Quiz) private readonly quizRepository: Repository<Quiz>,
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+    private readonly usersService: UsersService,
+  ) {}
+
+  async addReview(
+    quizId: string,
+    createReviewDto: CreateReviewDto,
+    req: Request,
+  ) {
+    const quiz = await this.quizRepository.findOne({
+      where: { id: quizId },
+      relations: ['creator'],
+    });
+    const user = await this.userRepository.findOne({
+      where: { id: req.user.id },
+    });
+
+    if (!quiz || !user) {
+      throw new Error('Quiz or user not found');
+    }
+
+    const review = this.reviewRepository.create({
+      quiz,
+      user,
+      ...createReviewDto,
+    });
+
+    await this.usersService.updateAuthorRating(review.quiz.creator.id);
+
+    return this.reviewRepository.save(review);
+  }
+
+  async getReviewsForQuiz(quizId: string) {
+    return this.reviewRepository.find({ where: { quiz: { id: quizId } } });
+  }
+}
