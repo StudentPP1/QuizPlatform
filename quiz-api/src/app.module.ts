694e0d40fb256811125577eb8d54d4ff7bb38744
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2025-03-12 18:51:23 +0200

    feat: set up app module

--- quiz-api/src/app.module.ts
+++ quiz-api/src/app.module.ts
@@ -1,6 +1,30 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { resolve } from 'path';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: resolve(process.cwd(), `.env.${process.env.NODE_ENV}.local`),
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: configService.get<string>('NODE_ENV') !== 'production',
+      }),
+    }),
+    AuthModule,
+  ],
 })
 export class AppModule {}
