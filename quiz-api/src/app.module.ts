1b2a5f7b8a66f1daf49f65cea0bdb4f70a32e2f5
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2025-03-13 15:11:04 +0200

    refactor: remove unnecessary module import

--- quiz-api/src/app.module.ts
+++ quiz-api/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { resolve } from 'path';
