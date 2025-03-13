9f3680e79f3371b21f5a47ff1467220bbc379115
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2025-03-13 15:16:28 +0200

    feat: set global prefix

--- quiz-api/src/main.ts
+++ quiz-api/src/main.ts
@@ -6,6 +6,7 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
 
