fd29dd32aa84e954143cc5237e6d38afb45a8677
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2025-03-14 09:21:06 +0200

    feat: add check user existing

--- quiz-api/src/users/users.service.ts
+++ quiz-api/src/users/users.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  ConflictException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
@@ -21,7 +25,14 @@ export class UsersService {
   }
 
   async createUser(createUserDto: CreateUserDto): Promise<User> {
-    const user = this.usersRepository.create(createUserDto);
-    return await this.usersRepository.save(user);
+    const existingUser = await this.usersRepository.findOneBy({
+      email: createUserDto.email,
+    });
+
+    if (existingUser)
+      throw new ConflictException('User with this email already exists');
+
+    const newUser = this.usersRepository.create(createUserDto);
+    return await this.usersRepository.save(newUser);
   }
 }
