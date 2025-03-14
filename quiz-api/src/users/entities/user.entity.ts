19cd16d6f4f4f527822c7b8b14692ee51d35d68b
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2025-03-14 10:06:50 +0200

    feat; update user entity

--- quiz-api/src/users/entities/user.entity.ts
+++ quiz-api/src/users/entities/user.entity.ts
@@ -3,6 +3,7 @@ import {
   PrimaryGeneratedColumn,
   Column,
   CreateDateColumn,
+  UpdateDateColumn,
 } from 'typeorm';
 
 @Entity('users')
@@ -10,18 +11,27 @@ export class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column({ type: 'varchar', length: '255' })
+  @Column({ type: 'varchar', length: 255 })
   username: string;
 
-  @Column({ type: 'varchar', length: '255' })
+  @Column({ type: 'varchar', length: 255 })
   email: string;
 
-  @Column({ type: 'varchar', length: '255' })
-  password: string;
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  password?: string;
+
+  @Column({ type: 'varchar', length: 255, nullable: true })
+  googleId?: string;
+
+  @Column({ type: 'varchar', length: 10 })
+  authProvider: string;
+
+  @Column({ type: 'varchar', length: 500, nullable: true })
+  avatarUrl?: string;
 
   @CreateDateColumn()
   createdAt: Date;
 
-  @CreateDateColumn()
+  @UpdateDateColumn()
   updatedAt: Date;
 }
