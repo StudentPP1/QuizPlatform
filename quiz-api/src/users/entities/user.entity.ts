a51cbe8864ab2a971a4d993fe6ba1289d0bcc055
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2025-04-08 17:27:36 +0300

    feat: update user entity

--- quiz-api/src/users/entities/user.entity.ts
+++ quiz-api/src/users/entities/user.entity.ts
@@ -1,20 +1,28 @@
 import {
   Entity,
-  PrimaryGeneratedColumn,
   Column,
+  PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
+  OneToMany,
+  ManyToMany,
+  JoinTable,
 } from 'typeorm';
 
-@Entity('users')
+import { QuizResult } from '@quiz/entities/quiz-result.entity';
+import { Quiz } from '@quiz/entities/quiz.entity';
+import { Review } from '@review/entities/review.entity';
+import { AuthProvider } from '@users/enum/auth-provider.enum';
+
+@Entity()
 export class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column({ type: 'varchar', length: 255 })
+  @Column({ type: 'varchar', length: 255, unique: true })
   username: string;
 
-  @Column({ type: 'varchar', length: 255 })
+  @Column({ type: 'varchar', length: 255, unique: true })
   email: string;
 
   @Column({ type: 'varchar', length: 255, nullable: true })
@@ -23,11 +31,27 @@ export class User {
   @Column({ type: 'varchar', length: 255, nullable: true })
   googleId?: string;
 
-  @Column({ type: 'varchar', length: 10 })
-  authProvider: string;
+  @Column({ type: 'enum', enum: AuthProvider })
+  authProvider: AuthProvider;
 
   @Column({ type: 'varchar', length: 500, nullable: true })
   avatarUrl?: string;
+
+  @OneToMany(() => Quiz, (quiz) => quiz.creator)
+  createdQuizzes: Quiz[];
+
+  @ManyToMany(() => Quiz, (quiz) => quiz.participants)
+  @JoinTable()
+  participatedQuizzes: Quiz[];
+
+  @OneToMany(() => QuizResult, (quizResult) => quizResult.user)
+  quizResults: QuizResult[];
+
+  @OneToMany(() => Review, (review) => review.user)
+  reviews: Review[];
+
+  @Column({ type: 'int', default: 0 })
+  rating: number;
 
   @CreateDateColumn()
   createdAt: Date;
