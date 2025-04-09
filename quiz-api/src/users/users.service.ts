93f085b20e290f0f76f3ef0df538cc970b759bae
Mykyta Sukhorukov <nikita.sukhorukoff@gmail.com>
2025-04-09 08:50:38 +0300

    feat: get a user info

--- quiz-api/src/users/users.service.ts
+++ quiz-api/src/users/users.service.ts
@@ -1,9 +1,15 @@
-import { Injectable, ConflictException } from '@nestjs/common';
+import {
+  Injectable,
+  ConflictException,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 
+import { QuizPreviewDto } from '@quiz/dto/quiz-preview.dto';
 import { CreateGoogleUserDto } from '@users/dto/create-google-user.dto';
 import { CreateUserDto } from '@users/dto/create-user.dto';
+import { UserInfoDto } from '@users/dto/user-info.dto';
 import { User } from '@users/entities/user.entity';
 import { AuthProvider } from '@users/enum/auth-provider.enum';
 
@@ -33,5 +39,35 @@ export class UsersService {
     });
 
     return await this.usersRepository.save(user);
+  }
+
+  async getUserInfo(userId: string): Promise<UserInfoDto> {
+    const user = await this.usersRepository.findOne({
+      where: { id: userId },
+      select: ['id', 'username', 'avatarUrl', 'rating', 'email'],
+      relations: ['createdQuizzes', 'participatedQuizzes'],
+    });
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    console.log(user);
+
+    const userInfo: UserInfoDto = {
+      userId: user.id,
+      username: user.username,
+      avatarUrl: user.avatarUrl,
+      rating: user.rating,
+      email: user.email,
+      participatedQuizzes:
+        user.participatedQuizzes?.map((quiz) => new QuizPreviewDto(quiz)) || [],
+      createdQuizzes:
+        user.createdQuizzes?.map((quiz) => new QuizPreviewDto(quiz)) || [],
+    };
+
+    console.log(userInfo);
+
+    return userInfo;
   }
 }
