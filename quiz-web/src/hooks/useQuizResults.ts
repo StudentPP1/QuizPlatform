56e20cd5bd82e348b5e82eeeb865e9b0e04f8b3f
StudentPP1 <mishamakytonin@gmail.com>
2025-04-21 23:18:31 +0300

    feat: add Task 1

--- quiz-web/src/hooks/useQuizResults.ts
+++ quiz-web/src/hooks/useQuizResults.ts
@@ -1,24 +1,27 @@
+import { QuizTask } from "./../models/QuizTask";
 import { useMemo } from "react";
-import { QuizTask } from "../models/QuizTask";
 import { UserAnswers } from "../models/UserAnswers";
 
 const useQuizResults = (quizTasks: QuizTask[], userAnswers: UserAnswers) => {
   return useMemo(() => {
     let correctUserAnswers = 0;
     let correctAnswers = 0;
+    
+    // TODO: Task 1 => implement for ... of loop to iterate over quizTasks
 
-    quizTasks.forEach((question) => {
+    for (const question of quizTasks) {
       const userAnswer = userAnswers[question.id];
       const correctAnswer = question.correctAnswers;
 
       correctAnswers += correctAnswer.length;
       if (userAnswer) {
-        correctUserAnswers += correctAnswer.filter((answer) =>
-          userAnswer.includes(answer)
-        ).length;
+        for (const answer of userAnswer) {
+          if (correctAnswer.includes(answer)) {
+            correctUserAnswers++;
+          }
+        }
       }
-    });
-
+    }
     return Math.round((correctUserAnswers / correctAnswers) * 100);
   }, [quizTasks, userAnswers]);
 };
