7daa6721e60d2d699406116bc8560786681831fc
Fererra <nikita.sukhorukoff@gmail.com>
2025-05-27 16:54:15 +0300

    chore: implement logging for review service

--- quiz-api/src/review/review.module.ts
+++ quiz-api/src/review/review.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 
 import {
+  BASE_REVIEW_SERVICE,
   REVIEW_REPOSITORY,
   REVIEW_SERVICE,
 } from '@common/constants/review.constants';
 import { QuizModule } from '@quiz/quiz.module';
+import { LoggingReviewDecorator } from '@review/logging-review.decorator';
 import { ReviewController } from '@review/review.controller';
 import { ReviewRepository } from '@review/review.repository';
 import { ReviewService } from '@review/review.service';
+import { IReviewService } from '@common/contracts/services/review.service.contract';
 import { UsersModule } from '@users/users.module';
 
 @Module({
@@ -19,8 +22,14 @@ import { UsersModule } from '@users/users.module';
       useClass: ReviewRepository,
     },
     {
-      provide: REVIEW_SERVICE,
+      provide: BASE_REVIEW_SERVICE,
       useClass: ReviewService,
+    },
+    {
+      provide: REVIEW_SERVICE,
+      useFactory: (baseService: IReviewService) =>
+        new LoggingReviewDecorator(baseService),
+      inject: [BASE_REVIEW_SERVICE],
     },
   ],
 })
