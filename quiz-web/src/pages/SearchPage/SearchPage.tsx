a34961b83443574a30ae883a54bd5bc98107f7ff
StudentPP1 <mishamakytonin@gmail.com>
2025-05-27 18:17:11 +0300

    perform: change style & delete import

--- quiz-web/src/pages/SearchPage/SearchPage.tsx
+++ quiz-web/src/pages/SearchPage/SearchPage.tsx
@@ -1,27 +1,48 @@
-import { FC, useRef } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import Wrapper from "../../components/wrapper/Wrapper";
 import styles from "./SearchPage.module.scss"
 import Avatar from "../../components/avatar/Avatar";
 import { QuizService } from "../../api/services/QuizService";
 import { QuizDTO } from "../../models/QuizDTO";
-import { usePaginatedData } from "../../hooks/usePaginatedFetch";
 import { useObserver } from "../../hooks/useObserver";
 import Loading from "../../components/loading/Loader";
+import { DEFAULT_PAGINATION_FROM, DEFAULT_PAGINATION_SIZE } from "../../constants/constants";
 
 const SearchPage: FC = () => {
   const navigate = useNavigate();
   const { text } = useParams<{ text: string }>();
   const lastElement = useRef<HTMLDivElement | null>(null);
+  const [quizzes, setQuizzes] = useState<QuizDTO[]>([])
+  const [isLoading, setLoading] = useState(false);
+  const [from, setFrom] = useState(DEFAULT_PAGINATION_FROM);
+  const [to, setTo] = useState(DEFAULT_PAGINATION_SIZE);
 
-  const { items: quizzes, isLoading } = usePaginatedData<QuizDTO>({
-    fetchFunction: QuizService.search,
-    observerTarget: lastElement,
-    id: text,
-    dependencies: [text],
-    useObserverHook: useObserver
-  });
+  useEffect(() => {
+    localStorage.setItem("index", "0");
+    const fetchData = async () => {
+      if (text) {
+        const data = await QuizService.search(from, to, text);
+        setQuizzes(data);
+        setFrom(to + 1);
+      }
+    };
 
+    fetchData();
+  }, [text]);
+
+  useObserver(lastElement, isLoading, async () => {
+    if (isLoading) return;
+    if (text == null) return;
+    setLoading(true);
+    await QuizService.search(from, to, text)
+      .then((data) => {
+        setQuizzes((prev) => [...prev, ...data]);
+        setFrom((prev) => prev + DEFAULT_PAGINATION_SIZE);
+        setTo((prev) => prev + DEFAULT_PAGINATION_SIZE);
+      })
+      .finally(() => setLoading(false));
+  })
 
   return (
     <Wrapper>
