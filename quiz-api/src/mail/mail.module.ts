cec4d056327c71972e10a72a59f885c74acbf71f
Fererra <nikita.sukhorukoff@gmail.com>
2025-05-28 18:12:23 +0300

    feat: implement logging for mail service

--- quiz-api/src/mail/mail.module.ts
+++ quiz-api/src/mail/mail.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 
+import {
+  BASE_MAIL_SERVICE,
+  MAIL_SERVICE,
+} from '@common/constants/mail.constants';
+import { IMailService } from '@common/contracts/services/mail.service.contract';
+import { LoggingMailDecorator } from '@mail/logging-mail.decorator';
 import { MailService } from '@mail/mail.service';
 
 @Module({
-  providers: [MailService],
-  exports: [MailService],
+  providers: [
+    { provide: BASE_MAIL_SERVICE, useClass: MailService },
+    {
+      provide: MAIL_SERVICE,
+      useFactory: (baseService: IMailService) =>
+        new LoggingMailDecorator(baseService),
+      inject: [BASE_MAIL_SERVICE],
+    },
+  ],
+  exports: [MAIL_SERVICE],
 })
 export class MailModule {}
