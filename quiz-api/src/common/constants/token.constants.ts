419224b4ab90340945627ee6ade0efa8168c05d0
Fererra <nikita.sukhorukoff@gmail.com>
2025-05-28 16:09:37 +0300

    feat: add new token

--- quiz-api/src/common/constants/token.constants.ts
+++ quiz-api/src/common/constants/token.constants.ts
@@ -1,2 +1,4 @@
 export const BASE_TOKEN_SERVICE = Symbol('BASE_TOKEN_SERVICE');
 export const TOKEN_SERVICE = Symbol('TOKEN_SERVICE');
+
+export const REFRESH_TOKEN_REPOSITORY = Symbol('REFRESH_TOKEN_REPOSITORY');
