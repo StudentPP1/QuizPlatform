620251a770e8c5e23b075342297edbdde8095b15
Fererra <nikita.sukhorukoff@gmail.com>
2025-05-28 16:11:32 +0300

    feat: provide refresh token repository

--- quiz-api/src/token/token.module.ts
+++ quiz-api/src/token/token.module.ts
@@ -3,10 +3,12 @@ import { JwtModule } from '@nestjs/jwt';
 
 import {
   BASE_TOKEN_SERVICE,
+  REFRESH_TOKEN_REPOSITORY,
   TOKEN_SERVICE,
 } from '@common/constants/token.constants';
 import { ITokenService } from '@common/contracts/services/token.service.contract';
 import { LoggingTokenDecorator } from '@token/logging-token.decorator';
+import { RefreshTokenRepository } from '@token/refresh-token.repository';
 import { TokenController } from '@token/token.controller';
 import { TokenService } from '@token/token.service';
 
@@ -23,6 +25,10 @@ import { TokenService } from '@token/token.service';
       useFactory: (baseService: ITokenService) =>
         new LoggingTokenDecorator(baseService),
       inject: [BASE_TOKEN_SERVICE],
+    },
+    {
+      provide: REFRESH_TOKEN_REPOSITORY,
+      useClass: RefreshTokenRepository,
     },
   ],
   exports: [TOKEN_SERVICE],
