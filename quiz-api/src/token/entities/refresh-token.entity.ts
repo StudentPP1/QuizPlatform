7fa479d424d8a27555de1fe268edab81ed11b8f0
Fererra <nikita.sukhorukoff@gmail.com>
2025-05-28 16:07:57 +0300

    feat: create refresh token entity

---
+++ quiz-api/src/token/entities/refresh-token.entity.ts
@@ -0,0 +1,30 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+import { User } from '@users/entities/user.entity';
+
+@Entity('refresh_tokens')
+export class RefreshToken {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => User)
+  user: User;
+
+  @Column()
+  tokenHash: string;
+
+  @Column()
+  expiresAt: Date;
+
+  @Column({ default: false })
+  isUsed: boolean;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
